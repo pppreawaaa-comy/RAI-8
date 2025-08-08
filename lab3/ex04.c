9c7c72cc0716da9af587f6c5cc57a2c168113e8b
pppreawaaa-comy <68230157@kmitl.ac.th>
2025-08-08 16:10:57 +0700

    Update ex04.c

--- lab3/ex04.c
+++ lab3/ex04.c
@@ -6,11 +6,9 @@ int main() {
     float average;
     char grade;
 
-    // รับชื่อผู้ใช้
     printf("Enter your name: ");
     scanf("%s", name);
 
-    // รับคะแนน
     printf("Enter your Calculus score: ");
     scanf("%f", &cal);
 
@@ -20,10 +18,8 @@ int main() {
     printf("Enter your Science score: ");
     scanf("%f", &sci);
 
-    // คำนวณค่าเฉลี่ย
     average = (cal + phy + sci) / 3.0;
 
-    // กำหนดเกรดตามช่วงคะแนน
     if (average >= 80)
         grade = 'A';
     else if (average >= 70)
@@ -33,9 +29,8 @@ int main() {
     else if (average >= 50)
         grade = 'D';
     else
-        grade = 'F';
-
-    // แสดงผลลัพธ์
+        grade = 'F'
+            
     printf("%s, your average is %.2f. You got grade %c.\n", name, average, grade);
 
     return 0;
