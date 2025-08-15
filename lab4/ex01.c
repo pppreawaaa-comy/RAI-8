0e235d0f6196d435b0a1aee3d12291ac795bb2c3
pppreawaaa-comy <68230157@kmitl.ac.th>
2025-08-15 06:24:39 +0000

    compelete all

--- lab4/ex01.c
+++ lab4/ex01.c
@@ -1,37 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int num1, num2, num3;
-    printf("Enter Num1: ");
-    scanf("%d", &num1);
-    printf("Enter Num2: ");
-    scanf("%d", &num2);
-
-    //แสดงการคำนวณ
-    printf("\nCalculator Menu:\n");
-    printf("1. +\n");
-    printf("2. -\n");
-    printf("3. *\n");
-    printf("4. /\n");
-    printf("5. %%\n");
-
-    printf("Choose menu: ");
-    scanf("%d", &menu);
-
-    //ใช้ switch เพื่อคำนวณ
-    switch(menu){
-        case 1 :
-        printf("Ans: %d + %d = %d\n", num1, num2, num1 + num2);
-        case 2 :
-        printf("Ans: %d - %d = %d\n", num1, num2, num1 - num2);
-        case 3 :
-        printf("Ans: %d * %d = %d\n", num1, num2, num1 * num2);
-        break;
-        case 4 :
-        if (num2 != 0)
-        printf("Ans : %d / %d = %.2f\n", num1, num2, (float)num1 /num2);
-    
-
-        }
-
+    printf("0\t");
+    printf("1\t");
+    printf("2\t");
+    printf("3\t");
+    printf("4\t");
+    printf("5\t");
+    printf("6\t");
+    printf("7\t");
+    printf("8\t");
+    printf("9\t");
+    printf("10\t");
+   
+    return 0;
 }
---
+++ lab4/ex02.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int count = 0;
+    printf("%d\t", count++);
+    printf("%d\t", count++);
+    printf("%d\t", count++);
+    printf("%d\t", count++);
+    printf("%d\t", count++);
+    printf("%d\t", count++);
+    printf("%d\t", count++);
+    printf("%d\t", count++);
+    printf("%d\t", count++);
+    printf("%d\t", count++);
+    return 0;
+}
