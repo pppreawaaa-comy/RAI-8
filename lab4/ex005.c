b0e89369907e37da1c069a6ac1033f1feda4dab7
pppreawaaa-comy <68230157@kmitl.ac.th>
2025-08-15 08:13:13 +0000

    r

---
+++ lab4/ex005.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() 
+{
+    int num,i;
+    int factorial = 1;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    for(i =1 ; i <= num; i++)
+    {
+        factorial*=i;
+    }
+    printf("Factorial of %d is %d\n", num, factorial);
+    return 0;
+}
+    
