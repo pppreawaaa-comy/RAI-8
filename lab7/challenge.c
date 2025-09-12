177b667a2e0c7d436dcbc97447539c3707ab3252
pppreawaaa-comy <68230157@kmitl.ac.th>
2025-09-12 09:38:05 +0000

    d

--- lab7/challenge.c
+++ lab7/challenge.c
@@ -1,13 +1,32 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+int main() 
 {
-    int array[5];
-    int *p = array;
+    int arr[5];
+    int *p = arr; 
     int i, j, temp;
 
     printf("Enter 5 integers: ");
-    for(i = 0; i < 5; i++)
+    for (i = 0; i < 5; i++) 
     {
-        scanf("%d", (p+i));
+        scanf("%d", (p + i));  
     }
+
+    for (i = 0; i < 5 - 1; i++) {
+        for (j = 0; j < 5 - i - 1; j++) {
+            if (*(p + j) > *(p + j + 1))
+             {
+                temp = *(p + j);
+                *(p + j) = *(p + j + 1);
+                *(p + j + 1) = temp;
+            }
+        }
+    }
+
+    printf("Sorted: ");
+    for (i = 0; i < 5; i++) {
+        printf("%d ", *(p + i));
+    }
+    printf("\n");
+
+    return 0;
 }
